43b39b92802f4f44192d6f6eba898255b7f97074
karolynnaran@gmail.com <karolynnaran@gmail.com>
2025-06-10 14:43:29 -0400

    yippee

--- domManipulation.js
+++ domManipulation.js
@@ -14,6 +14,9 @@ let image = document.querySelector('.image');
 // 1. An event listener is provided for the `contentButton`
 // 2. When clicked, it should change the content of:
 //   - The `name` header.
+contentButton.addEventListener('click', function() {
+    myName.innerHTML = "karo";
+});
 
 // -- RETURN TO LESSON --
 
@@ -22,24 +25,24 @@ let image = document.querySelector('.image');
 //   - The `location` paragraph.
 
 contentButton.addEventListener('click', function() {
-
-
-
-
-
-
+    myHobby.innerHTML = 'soccer';
+    myLocation.innerHTML = 'indy';
 });
 
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
 
-
+image1Button.addEventListener('click', function() {
+    image.src = 'image (1).png';
+});
 
 
 
 // 5. Add an event listener to the `image2Button`
 // 6. When clicked, it should change the source of `image` to another image URL.
-
+image2Button.addEventListener('click', function() {
+    image.src = 'https://media.tenor.com/J8D7WFWX4pIAAAAe/listening-music-musical-notes.png';
+});
 
 // BONUS
 // 7. Try using different event types for your buttons.
--- script.js
+++ script.js
@@ -4,15 +4,16 @@ let img = document.querySelector("img");
 
 // 1. Press the button on the webpage to change it up! Take notice of what happens.
 // - What do you think innerHTML does?
+// it gets the code within html and change it
 // - What do you think .src does?
-
+//source attribute
 
 // 2. Try the following:
 // - Make it so that the header says your name when the button is clicked.
 // - Change the src of the image to something random.
 button.addEventListener("click", function() {
-    header.innerHTML = "🤯 wow cool neeto";
-    img.src = "https://media4.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif";
+    header.innerHTML = "🤯 karo!";
+    img.src = "https://media.tenor.com/MKkJWYigjycAAAAM/cute-cat-cat-cute.gif";
 });
 
 
